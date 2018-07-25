62aace192f5fadfd03c55279950b250806f6a6bd
Vladica <vladica.aleksic84@gmail.com>
2018-07-25 14:14:00 +0200

    Some changes

--- Portfolio/kod.js
+++ Portfolio/kod.js
@@ -22,3 +22,34 @@
     });
   }).scroll(); //invoke scroll-handler on page-load
 });
+
+
+//adding class by click
+$(document).ready(function() {
+    $('li a').click(function(event) {
+    var offset = $($(this).attr("href")).offset().top;
+    $('html, body').animate({
+      scrollTop: offset + 'px'
+    },500);     
+    $('li a').removeClass('active');
+    $(this).addClass('active')
+    event.preventDefault();
+  });
+});
+
+
+
+   
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+  
+    // Assign active class to nav links while scolling
+    $('.page').each(function(i) {
+        if ($(this).position().top <= scrollDistance ) {
+            $('.navigation a.active').removeClass('active');
+            $('.navigation a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
