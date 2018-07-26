491aa0f3c3d0682800d44b71b5f4997c151d00f7
Vladica <vladica.aleksic84@gmail.com>
2018-07-26 11:37:16 +0200

    responsivness

--- Portfolio/kod.js
+++ Portfolio/kod.js
@@ -53,3 +53,9 @@ $(window).scroll(function() {
         }
     });
 }).scroll();
+
+
+//removing advertise for webhost
+$(document).ready(function(){
+  $('body').find('img[src$="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"]').remove();
+})
